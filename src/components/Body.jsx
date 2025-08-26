ac64a05d0daf77e7d5cf946c8b29f458085b03f8
Godson Saji <godsonsaji077@gmail.com>
2025-08-26 15:36:29 +0530

    add files

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,16 +1,33 @@
 import ResCard from "./ResCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfRes, setListOfRes] = useState(resList);
   return (
     <div className="body">
-      <div className="search-container">
-        <input type="text-box" placeholder="Biriyani"></input>
-        <button>Search</button>
+      <div className="mid-container">
+        <div className="btn-container">
+          <button
+            onClick={() => {
+              let filteredList = listOfRes.filter(
+                (res) => res.card.card.info.avgRating >= 4.5
+              );
+              setListOfRes(filteredList);
+              console.log(filteredList);
+            }}
+          >
+            Top Rated
+          </button>
+        </div>
+        <div className="search-container">
+          <input type="text-box" placeholder="Biriyani"></input>
+          <button>Search</button>
+        </div>
       </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
-          <ResCard key={restaurant.info.id} resData={restaurant} />
+        {listOfRes.map((restaurant) => (
+          <ResCard key={restaurant.card.card.info.id} resData={restaurant} />
         ))}
       </div>
     </div>
--- src/components/ResCard.jsx
+++ src/components/ResCard.jsx
@@ -2,8 +2,8 @@ import { IMAGE_URL } from "../utils/constants";
 const ResCard = (props) => {
   const { resData } = props;
   const { name, avgRatingString, cuisines, locality, cloudinaryImageId } =
-    resData.info;
-  const { slaString } = resData.info.sla;
+    resData.card.card.info;
+  const { slaString } = resData.card.card.info.sla;
   return (
     <div className="card-container">
       <img src={IMAGE_URL + cloudinaryImageId}></img>
